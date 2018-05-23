1de1e8999c4ce01b905b0514666c6780a3ba3f66
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-05-23 12:46:56 -0400

    in-progress

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var $country = document.querySelector('#country');
 var $shape = document.querySelector('#shape');
 
 var alien_data = dataSet;
+var temporary_alien_data;
 
 render_table();
 
@@ -25,8 +26,8 @@ function render_table() {
     }
 
     // setting up a table
-    // for (i=0; i < alien_data.length; i++) {
-    for (i=0; i < 200; i++) {
+    for (i=0; i < alien_data.length; i++) {
+    // for (i=0; i < 200; i++) {
         var $bodyrow = $tablebody.insertRow(i);
         for (j=0; j < Object.keys(alien_data[i]).length; j++) {
             var $bodycell = $bodyrow.insertCell(j);
@@ -37,12 +38,58 @@ function render_table() {
 
 // filtering date/time
 function searching() {
-    alien_data = dataSet.filter(d => { return d.datetime === $date.value; });
+
+    var dateInput = $date.value.trim(); //need regexp for dates 
+    var cityInput = $city.value.trim().toLowerCase();
+    var stateInput = $state.value.trim().toLowerCase();
+    var countryInput = $country.value.trim().toLowerCase();
+    var shapeInput = $shape.value.trim().toLowerCase();
+
+    alien_data = dataSet.filter(function(date) {
+        var lowdate = date.datetime.toLowerCase();
+        return date.datetime === dateInput;
+    });
+    if ($city.value) {
+        alien_data = alien_data.filter(c => { return c.city === cityInput; });
+    }
+    if ($state.value) {
+        alien_data = alien_data.filter(s => { return s.state === stateInput; });
+    }
+
+    // var temporary_alien_data = '';
+    // temporary_alien_data = alien_data.filter(c => { return c.city === cityInput; });
+
+    // alien_data = temporary_alien_data;
     render_table()
+    // alien_data = dataSet;
 }
 
 $searchbutton.addEventListener('click', searching);
 
+function multisearch() {
+
+    var dateInput = $date.value.trim(); //need regexp for dates 
+    var cityInput = $city.value.trim().toLowerCase();
+    var stateInput = $state.value.trim().toLowerCase();
+    var countryInput = $country.value.trim().toLowerCase();
+    var shapeInput = $shape.value.trim().toLowerCase();
+
+    var criterias = {
+        datetime: dateInput,
+        city: cityInput,
+        state: stateInput,
+        country: countryInput,
+        shape: shapeInput
+    }
+
+    alien_data = dataSet.filter(function(obj) {
+        return Object.keys(criterias).every(function(c) {
+            return obj[c] == criterias[c];
+        });
+    });
+
+    render_table();
+}
 
 
 

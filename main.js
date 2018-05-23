b2396ed690500ca87646829062432ace7394de33
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-05-22 22:46:36 -0400

    in-progress

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 var $tablehead = document.querySelector('#table-head');
 var $tablebody = document.querySelector('#table-body');
 
+var $searchbutton = document.querySelector('#search');
+
+var $date = document.querySelector('#date-time');
+var $city = document.querySelector('#city');
+var $state = document.querySelector('#state');
+var $country = document.querySelector('#country');
+var $shape = document.querySelector('#shape');
 
 var alien_data = dataSet;
 
-// setting up a table header
+render_table();
 
-var $headrow = $tablehead.insertRow(0);
-$tablehead.innerHTML = '';
-for (i=0; i < Object.keys(alien_data[0]).length; i++) {
-    var $headcell = $headrow.insertCell(i);
-    $headcell.innerText = Object.keys(alien_data[0])[i];
-}
+function render_table() {
+    $tablehead.innerHTML = '';
+    $tablebody.innerHTML = '';
 
-// setting up a table
+    // setting up a table header
+    var $headrow = $tablehead.insertRow(0);
+    for (i=0; i < Object.keys(alien_data[0]).length; i++) {
+        var $headcell = $headrow.insertCell(i);
+        $headcell.innerText = Object.keys(alien_data[0])[i];
+    }
 
-$tablebody.innerHTML = '';
-for (i=0; i < alien_data.length; i++) {
-    var $bodyrow = $tablebody.insertRow(i);
-    for (j=0; j < Object.keys(alien_data[i]).length; j++) {
-        var $bodycell = $bodyrow.insertCell(j);
-        $bodycell.innerText = Object.values(alien_data[i])[j];
+    // setting up a table
+    // for (i=0; i < alien_data.length; i++) {
+    for (i=0; i < 200; i++) {
+        var $bodyrow = $tablebody.insertRow(i);
+        for (j=0; j < Object.keys(alien_data[i]).length; j++) {
+            var $bodycell = $bodyrow.insertCell(j);
+            $bodycell.innerText = Object.values(alien_data[i])[j];
+        }
     }
 }
+
+// filtering date/time
+function searching() {
+    alien_data = dataSet.filter(d => { return d.datetime === $date.value; });
+    render_table()
+}
+
+$searchbutton.addEventListener('click', searching);
+
+
+
 
 
 

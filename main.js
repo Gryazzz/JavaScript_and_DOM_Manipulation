ed152378a9061f52cb1f92a34649b7f40e2518ca
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-05-25 08:29:35 -0400

    in-progress

--- main.js
+++ main.js
@@ -13,9 +13,14 @@ var $shape = document.querySelector('#shape');
 
 var alien_data = dataSet;
 var chunkdata = dataSet;
+
+//pagination variables
 var currentPage = 1;
+var perPage = 10;
+var numberOfPages = Math.ceil(alien_data.length / perPage);
 
 // render_table();
+render_table_chunk();
 
 function render_table() {
     $tablehead.innerHTML = '';
@@ -32,7 +37,6 @@ function render_table() {
 
         // setting up a table
         for (i=0; i < alien_data.length; i++) {
-        // for (i=0; i < 200; i++) {
             var $bodyrow = $tablebody.insertRow(i);
             for (j=0; j < Object.keys(alien_data[i]).length; j++) {
                 var $bodycell = $bodyrow.insertCell(j);
@@ -97,17 +101,14 @@ function refresh() {
     $country.value = '';
     $shape.value = '';
 
-    render_table();
+    render_table_chunk();
 }
 
-
+//render data by chunks
 function render_table_chunk() {
 
     $tablehead.innerHTML = '';
     $tablebody.innerHTML = '';
-
-    var perPage = 10;
-    var numberOfPages = Math.ceil(alien_data.length / perPage);
     
     chunkdata = alien_data.slice((currentPage-1) * perPage, currentPage * perPage);
 
@@ -129,7 +130,7 @@ function render_table_chunk() {
             })
         });   
     }
-    
+
     catch (error) {
         console.log('NO data in the dataset');
         var $headrow = $tablehead.insertRow(0);
@@ -138,113 +139,39 @@ function render_table_chunk() {
     }
 }
 
+function nextPage() {
 
-render_table_chunk();
+    if (currentPage === numberOfPages) {
+        currentPage = currentPage;
+    }
+    else {
+        currentPage++;
+        render_table_chunk();
+    }
+}
 
+function previousPage() {
 
+    if (currentPage === 1) {
+        currentPage = currentPage;
+    }
+    else {
+        currentPage--;
+        render_table_chunk();
+    }
+}
 
-//Pagination
+function firstPage(){
+    currentPage = 1;
+    render_table_chunk();
+}
 
-    // var list = alien_data;
-    // var pageList = new Array();
-    // var currentPage = 1;
-    // var numberPerPage = 100; //number of records per page
-    // var numberOfPages = Math.ceil(alien_data.length / numberPerPage);
-
-    // function nextPage() {
-    //     currentPage += 1;
-    //     loadList();
-    // }
-
-    // function previousPage() {
-    //     currentPage -= 1;
-    //     loadList();
-    // }
-
-    // function firstPage() {
-    //     currentPage = 1;
-    //     loadList();
-    // }
-
-    // function lastPage() {
-    //     currentPage = numberOfPages;
-    //     loadList();
-    // }
-
-    // function loadList() {
-    //     // var pageList = new Array();
-    //     // var currentPage = 1;
-    //     // var numberPerPage = 100; //number of records per page
-    //     // var numberOfPages = Math.ceil(alien_data.length / numberPerPage);
-    //     var begin = ((currentPage - 1) * numberPerPage);
-    //     var end = begin + numberPerPage;
-    
-    // pageList = alien_data.slice(begin, end);
-
-    //     try {
-    //         var $headrow = $tablehead.insertRow(0);
-    //         for (i=0; i < Object.keys(pageList[0]).length; i++) {
-    //             var $headcell = $headrow.insertCell(i);
-    //             $headcell.innerText = Object.keys(pageList[0])[i];
-    //         }
-
-    //         // setting up a table
-    //         for (i=0; i < n; i++) {
-    //             var $bodyrow = $tablebody.insertRow(i);
-    //             for (j=0; j < Object.keys(pageList[i]).length; j++) {
-    //                 var $bodycell = $bodyrow.insertCell(j);
-    //                 $bodycell.innerText = Object.values(pageList[i])[j];
-    //             }
-    //         }
-    //     }
-    //     catch (error) {
-    //         console.log('NO data in the dataset');
-    //         var $headrow = $tablehead.insertRow(0);
-    //         var $headcell = $headrow.insertCell(0);
-    //         $headcell.innerText = 'Sorry we don\'t have the data you\'ve requested. Please do another search.';
-    //     }
-    // }
-
-    // function check() {
-    //         document.querySelector("#next").disabled = currentPage == numberOfPages ? true : false;
-    //         document.querySelector("#previous").disabled = currentPage == 1 ? true : false;
-    //         document.querySelector("#first").disabled = currentPage == 1 ? true : false;
-    //         document.querySelector("#last").disabled = currentPage == numberOfPages ? true : false;
-    //     }
-    // check();         // determines the states of the pagination buttons
+function lastPage() {
+    currentPage = numberOfPages;
+    render_table_chunk();
+}
 
 
-
-    // while(p) { //current page number
-        
-    //     $tablehead.innerHTML = '';
-    //     $tablebody.innerHTML = '';
-
-    //     var chunkdata = alien_data.slice(p*n, (p+1)*n);
-
-    //     try {
-    //         var $headrow = $tablehead.insertRow(0);
-    //         for (i=0; i < Object.keys(alien_data[0]).length; i++) {
-    //             var $headcell = $headrow.insertCell(i);
-    //             $headcell.innerText = Object.keys(alien_data[0])[i];
-    //         }
-
-    //         // setting up a table
-    //         for (i=0; i < n; i++) {
-    //             var $bodyrow = $tablebody.insertRow(i);
-    //             for (j=0; j < Object.keys(chunkdata[i]).length; j++) {
-    //                 var $bodycell = $bodyrow.insertCell(j);
-    //                 $bodycell.innerText = Object.values(chunkdata[i])[j];
-    //             }
-    //         }
-    //     }
-    //     catch (error) {
-    //         console.log('NO data in the dataset');
-    //         var $headrow = $tablehead.insertRow(0);
-    //         var $headcell = $headrow.insertCell(0);
-    //         $headcell.innerText = 'Sorry we don\'t have the data you\'ve requested. Please do another search.';
-    //     }
-    // }
 
 
 

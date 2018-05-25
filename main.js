41b6a48d7e1d2bef5eb098d5dd4b20e07a22222f
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-05-25 11:30:08 -0400

    in-progress

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
-var $tablehead = document.querySelector('#table-head');
-var $tablebody = document.querySelector('#table-body');
-var $currentpage = document.querySelector('.curpage');
+// var $tablehead = document.querySelector('#table-head');
+var $tablehead = d3.select('#table-head');
+// var $tablebody = document.querySelector('#table-body');
+var $tablebody = d3.select('#table-body');
+var $currentpage = d3.select('.curpage');
 
-var $searchbutton = document.querySelector('#search');
-var $refreshbutton = document.querySelector('#refresh');
-
-var $date = document.querySelector('#date-time');
-var $city = document.querySelector('#city');
-var $state = document.querySelector('#state');
-var $country = document.querySelector('#country');
-var $shape = document.querySelector('#shape');
+var $searchbutton = d3.select('#search');
+var $refreshbutton = d3.select('#refresh');
 
 var alien_data = dataSet;
 var chunkdata = alien_data;
@@ -19,71 +15,44 @@ var currentPage = 1;
 var perPage = 10;
 var numberOfPages = Math.ceil(alien_data.length / perPage);
 
-
 render_table_chunk();
 
-// function render_table() {
-//     $tablehead.innerHTML = '';
-//     $tablebody.innerHTML = '';
-
-//     // setting up a table header
-
-//     try {
-//         var $headrow = $tablehead.insertRow(0);
-//         for (i=0; i < Object.keys(alien_data[0]).length; i++) {
-//             var $headcell = $headrow.insertCell(i);
-//             $headcell.innerText = Object.keys(alien_data[0])[i];
-//         }
-
-//         // setting up a table
-//         for (i=0; i < alien_data.length; i++) {
-//             var $bodyrow = $tablebody.insertRow(i);
-//             for (j=0; j < Object.keys(alien_data[i]).length; j++) {
-//                 var $bodycell = $bodyrow.insertCell(j);
-//                 $bodycell.innerText = Object.values(alien_data[i])[j];
-//             }
-//         }
-//     }
-//     catch (error) {
-//         console.log('NO data in the dataset');
-//         var $headrow = $tablehead.insertRow(0);
-//         var $headcell = $headrow.insertCell(0);
-//         $headcell.innerText = 'Sorry we do not have the data you have requested. Please refresh the page and do another search.';
-//     }
-// }
+$refreshbutton.on('click', refresh);
+$searchbutton.on('click', searching);
 
 // multisearch function
 
 function searching() {
 
-    var dateInput = $date.value.trim(); //need regexp for dates 
-    var cityInput = $city.value.trim().toLowerCase(); //need regexp for partial entry
-    var stateInput = $state.value.trim().toLowerCase();
-    var countryInput = $country.value.trim().toLowerCase();
-    var shapeInput = $shape.value.trim().toLowerCase();
+    var dateInput = d3.select("#date-time").node().value; //need regexp for dates 
+    var cityInput = d3.select("#city").node().value.trim().toLowerCase(); //need regexp for partial entry
+    var stateInput = d3.select('#state').node().value.trim().toLowerCase();
+    var countryInput = d3.select('#country').node().value.trim().toLowerCase();
+    var shapeInput = d3.select('#shape').node().value.trim().toLowerCase();
+
     currentPage = 1;
 
 
-    if ($date.value) {
+    if (dateInput) {
         alien_data = dataSet.filter(function(date) {
             
             return date.datetime === dateInput;
     });
     }
     
-    if ($city.value) {
+    if (cityInput) {
         alien_data = alien_data.filter(c => { return c.city.toLowerCase() === cityInput; }); //key name can be changed to automatic one. one day
     }
         
-    if ($state.value) {
+    if (stateInput) {
         alien_data = alien_data.filter(s => { return s.state.toLowerCase() === stateInput; });
     }
 
-    if ($country.value) {
+    if (countryInput) {
         alien_data = alien_data.filter(c => { return c.country.toLowerCase() === countryInput; });
     }
 
-    if ($shape.value) {
+    if (shapeInput) {
         alien_data = alien_data.filter(s => { return s.shape.toLowerCase() === shapeInput; });
     }
 
@@ -91,18 +60,18 @@ function searching() {
     numberOfPages = Math.ceil(alien_data.length / perPage);   
 }
 
-$searchbutton.addEventListener('click', searching);
-$refreshbutton.addEventListener('click', refresh);
 
 // Refresh table data
 function refresh() {
     alien_data = dataSet;
 
-    $city.value = '';
-    $date.value = '';
-    $state.value = '';
-    $country.value = '';
-    $shape.value = '';
+    d3.select('.form-control').nodes().value = '';
+
+    // cityInput.value('');
+    // dateInput = '';
+    // stateInput = '';
+    // countryInput = '';
+    // shapeInput = '';
 
     render_table_chunk();
     numberOfPages = Math.ceil(alien_data.length / perPage);
@@ -111,37 +80,39 @@ function refresh() {
 //render data by chunks
 function render_table_chunk() {
 
-    $tablehead.innerHTML = '';
-    $tablebody.innerHTML = '';
+    $tablehead.text('');
+    $tablebody.text('');
     
     chunkdata = alien_data.slice((currentPage-1) * perPage, currentPage * perPage);
 
     $currentpage.innerText = currentPage;
 
     try {
+        var $headrow = $tablehead.append("tr");
+        Object.keys(alien_data[0]).forEach(item => $headrow.append('td').text(item));
+
 
         //setting up a header
-        var $headrow = $tablehead.insertRow(0);
-        Object.keys(alien_data[0]).forEach(function(item, index) {
-            var $headcell = $headrow.insertCell(index);
-            $headcell.innerText = item;
-        });
+        // var $headrow = $tablehead.insertRow(0);
+        // Object.keys(alien_data[0]).forEach(function(item) {
+        //     var $headcell = $headrow.append('td').text(item);
+            // $headcell.innerText = item;
 
         // setting up a table
-        chunkdata.forEach(function(item, index) {
-            var $bodyrow = $tablebody.insertRow(index);
-            Object.values(item).forEach(function(item, index) {
-                var $bodycell = $bodyrow.insertCell(index);
-                $bodycell.innerText = item;
-            })
-        });   
-    }
+        chunkdata.forEach(function(item) {
+            var $bodyrow = $tablebody.append('tr');
+            Object.values(item).forEach(value => $bodyrow.append('td').text(value));
+            
+            // var $bodyrow = $tablebody.insertRow(index);
+                // var $bodycell = $bodyrow.insertCell(index);
+                // var $bodycell = ;
+                // $bodycell.innerText = item;
+            });
+        }
 
     catch (error) {
         console.log('NO data in the dataset');
-        var $headrow = $tablehead.insertRow(0);
-        var $headcell = $headrow.insertCell(0);
-        $headcell.innerText = 'Sorry we do not have the data you have requested. Please refresh the page and do another search.';
+        $tablehead.append('tr').append('td').text('Sorry we do not have the data you have requested. Please refresh the page and do another search.');
     }
 }
 
